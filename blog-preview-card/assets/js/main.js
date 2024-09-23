d82d86a352b95156f9748fe47c99bc94a6f0601e
Guilherme Ferreti <guiferretis@gmail.com>
2024-09-23 13:05:01 -0300

    Improved add random tag logic

--- blog-preview-card/assets/js/main.js
+++ blog-preview-card/assets/js/main.js
@@ -4,21 +4,21 @@ const tagsContainer = document.querySelector('.preview-card__tags');
 const tagNames = ['Blog', 'HTML & CSS', 'Code', 'Tutorial'];
 
 const addRandomTag = () => {
-    const tagName = tagNames.pop();
-
-    if (!tagName) {
-        tagsContainer.querySelectorAll('.preview-card__tag').forEach(tag => tag.style.cursor = 'default');
-
+    if (tagNames.length === 0) {
         return;
     }
 
     const newTag = document.createElement('span');
 
     newTag.classList.add('preview-card__tag');
-    newTag.textContent = tagName;
+    newTag.textContent = tagNames.pop();
     newTag.addEventListener('click', addRandomTag);
 
     tagsContainer.appendChild(newTag);
+
+    if (tagNames.length === 0) {
+        tagsContainer.querySelectorAll('.preview-card__tag').forEach(tag => tag.style.cursor = 'default');
+    }
 }
 
 tagsContainer.querySelectorAll('.preview-card__tag').forEach(tag => tag.addEventListener('click', addRandomTag));
